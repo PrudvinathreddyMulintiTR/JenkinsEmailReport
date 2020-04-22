8c2d5b90b5361d4a5afedc3c5cdd74f4fe811908
UC221101 <prudvinathreddy.mulinti.thomsonreuters.com>
2020-04-22 17:12:49 -0400

    :prudvi: Removed a variable

--- src/tr/qa/Main.java
+++ src/tr/qa/Main.java
@@ -349,6 +349,7 @@ public class Main {
                     if (el.getName() == "result") {
                         // System.out.println( el.getText() );
                         if (el.getText().contains("FAILURE")) {
+                            System.out.println("result:", el.getText());
                             // failures += 1;
 
 //                            TODO: Need to add detailed report.
@@ -358,6 +359,7 @@ public class Main {
                             emailContent.append("<tr><td>" + counter + "</td><td><b>" + dt.toString() + "</b> </td><td><b><font color='red'>" + el.getText() + "</font></b></td></tr>");
                             //detailedReport(dt);
                         } else if (el.getText().contains("UNSTABLE")) {
+                            System.out.println("result:", el.getText());
                             // unstable += 1;
 //                            emailContent.append( "<b><font color='SteelBlue'>" + el.getText() + "</font></b>" );
 //                            String TnEU = detailedReport(dt);
@@ -366,6 +368,7 @@ public class Main {
                             emailContent.append("<tr><td>" + counter + "</td><td><b>" + dt.toString() + "</b></td><td><b><font color='SteelBlue'>" + el.getText() + "</font></b></td></tr>");
 
                         } else if (el.getText().contains("SUCCESS")) {
+                            System.out.println("result:", el.getText());
                             //emailContent.append( "<b><font color='green'>" + el.getText() + "</font></b>" );
                             emailContent.append("<tr><td>" + counter + "</td><td><b>" + dt.toString() + "</b></td><td><b><font color='green'>" + el.getText() + "</font></b></td><td ></td></tr>");
                         }
@@ -389,8 +392,8 @@ public class Main {
         emailContent.append( "<h3>Failures: " + failures + "</h3>" );
         emailContent.append( "<h3>Running Jobs: " + isRunning + "</h3>" );
         emailContent.append( "<h3>Unstable jobs: " + unstable + "</h3>" );*/
-//        emailContent.append("<br/><br/>ERRORS(if any):<br/><br>");
-//        emailContent.append("<i>" + errorFooter + "</i><br/><br/>");
+        emailContent.append("<br/><br/>ERRORS(if any):<br/><br>");
+        emailContent.append("<i>" + errorFooter + "</i><br/><br/>");
         emailContent.append("-----------------------------------<br/><i>NOTE: To change the look of this report, edit the Main.java in this test report project.  To<br/>change the list of Jenkins jobs handled by this report, edit the DefinedTests.java .</i>");
 
     }
